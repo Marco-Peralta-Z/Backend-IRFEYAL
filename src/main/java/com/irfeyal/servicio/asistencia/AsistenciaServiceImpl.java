e6001445f5ae25e24992faab03596d5273c02f51
Stevensmb <100179543+Stevensmb@users.noreply.github.com>
2022-04-15 18:17:25 -0500

    creacion clases services mod Asistencia

---
+++ src/main/java/com/irfeyal/servicio/asistencia/AsistenciaServiceImpl.java
@@ -0,0 +1,142 @@
+package com.irfeyal.servicio.asistencia;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.irfeyal.modelo.asistencia.Asistencia;
+import com.irfeyal.modelo.dao.asistencia.IAsistenciaDao;
+import com.irfeyal.modelo.dao.asistencia.IClaseDao;
+import com.irfeyal.modelo.dao.asistencia.IEstudianteDao;
+import com.irfeyal.modelo.dao.asistencia.IPersonaDao;
+import com.irfeyal.modelo.matricula.Estudiante;
+import com.irfeyal.modelo.parametrizacionacademica.Modalidad;
+import com.irfeyal.modelo.rolseguridad.Persona;
+
+public class AsistenciaServiceImpl implements IAsistenciaService{
+	
+	@Autowired
+	private IAsistenciaDao asistenciadao;
+	@Autowired
+	private IPersonaDao personadao;
+	@Autowired
+	private IEstudianteDao estudiantedao;
+	@Autowired
+	private IClaseDao clasedao;
+	
+
+	@Override
+	public List<Asistencia> findAll() {
+		// TODO Auto-generated method stub
+		return (List<Asistencia>) asistenciadao.findAll();
+	}
+
+	@Override
+	public Asistencia findById(Long id) {
+		// TODO Auto-generated method stub
+		return asistenciadao.findById(id).orElse(null);
+	}
+
+	@Override
+	public Asistencia save(Asistencia asistencia) {
+		// TODO Auto-generated method stub
+		return asistenciadao.save(asistencia);
+	}
+
+	@Override
+	public void delete(Long id) {
+		asistenciadao.deleteById(id);
+		
+	}
+
+	@Override
+	public List <Persona> buscarcurso(Long id) {
+		// TODO Auto-generated method stub
+		return personadao.buscarcurso(id);
+	}
+
+	@Override
+	public List<Estudiante> buscarcursomodalidad(Long id_mod, Long id_periodo, Long id_paralelo, Long id_asignatura) {
+		
+		
+			if ( id_periodo==0 && id_paralelo==0 && id_asignatura==0) {
+				return estudiantedao.buscarcursomodalidad(id_mod);
+				
+			}else {
+				if ( id_mod==0 && id_paralelo==0 && id_asignatura==0) {
+					return estudiantedao.buscarcursoperiodo(id_periodo);
+					
+				}else {
+					if ( id_mod==0 && id_periodo==0 && id_asignatura==0) {
+						return estudiantedao.buscarcursoparalelo(id_paralelo);
+						
+					}else {
+						if ( id_mod==0 && id_periodo==0 && id_paralelo==0 ) {
+							return estudiantedao.buscarcursoasignatura(id_asignatura);
+							
+						}else {
+							
+							
+							if (id_asignatura==0 && id_paralelo==0) {
+								return estudiantedao.buscarcursomodperi(id_mod, id_periodo);
+							}else {
+								if (id_mod==0 && id_periodo==0) {
+									return estudiantedao.buscarcursoparasig(id_paralelo, id_asignatura);
+								}else {
+									if (id_mod==0 && id_asignatura==0) {
+										return estudiantedao.buscarcursoperipar(id_periodo, id_paralelo);
+									}else {
+										if (id_mod==0 && id_paralelo==0) {
+											return estudiantedao.buscarcursoperiasig(id_periodo, id_asignatura);
+										}else {
+											if (id_periodo==0 && id_paralelo==0) {
+												return estudiantedao.buscarcursomodasig(id_mod, id_asignatura);
+											}else {
+												if (id_periodo==0 && id_asignatura==0) {
+													return estudiantedao.buscarcursomodpar(id_mod, id_paralelo);
+												}else {
+													
+													
+													if (id_asignatura==0 ) {
+														return estudiantedao.buscarcursomodperpar(id_mod, id_periodo,id_paralelo);
+													}else {
+														if (id_paralelo==0 ) {
+															return estudiantedao.buscarcursomodpersig(id_mod, id_periodo,id_asignatura);
+														}else {
+															if (id_periodo==0 ) {
+																return estudiantedao.buscarcursomodparasig(id_mod, id_paralelo,id_asignatura);
+															}else {
+																if (id_mod==0 ) {
+																	return estudiantedao.buscarcursoperparasig( id_periodo,id_paralelo,id_asignatura);
+																}else {
+																	 return estudiantedao.buscarcursomodalidad(id_mod, id_periodo, id_paralelo, id_asignatura);
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+   }
+
+	@Override
+	public Estudiante buscarceduestudiante(String cedula) {
+		
+		return estudiantedao.buscarcedulaestudiante(cedula);
+	}
+
+	@Override
+	public List<Modalidad> modalidaddocente(Integer id_docente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/irfeyal/servicio/asistencia/IAsistenciaService.java
@@ -0,0 +1,24 @@
+package com.irfeyal.servicio.asistencia;
+
+import java.util.List;
+
+import com.irfeyal.modelo.asistencia.Asistencia;
+import com.irfeyal.modelo.matricula.Estudiante;
+import com.irfeyal.modelo.parametrizacionacademica.Modalidad;
+import com.irfeyal.modelo.rolseguridad.Persona;
+
+
+
+public interface IAsistenciaService {
+	
+	public List<Asistencia> findAll();
+	public Asistencia findById(Long id);
+	public Asistencia save(Asistencia asistencia);
+	public void delete(Long id);
+	public List <Persona> buscarcurso(Long id);
+	public List <Estudiante> buscarcursomodalidad(Long id_mod,Long id_periodo,Long id_paralelo,Long id_asignatura);
+	public Estudiante buscarceduestudiante(String cedula);
+	
+	public List<Modalidad> modalidaddocente(Integer id_docente);
+	
+}
