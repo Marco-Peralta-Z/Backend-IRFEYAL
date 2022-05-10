dbbd7de6c378ef6c2a686fe056acd2909150ba31
JaviKo500 <javikogutierrez64@gmail.com>
2022-05-10 15:58:47 -0500

    agrego query para buscar por nombre de usuario y su estado

--- src/main/java/com/irfeyal/modelo/dao/rolseguridad/UsuarioDAO.java
+++ src/main/java/com/irfeyal/modelo/dao/rolseguridad/UsuarioDAO.java
@@ -11,6 +11,14 @@ import com.irfeyal.modelo.rolseguridad.Usuario;
 public interface UsuarioDAO extends CrudRepository<Usuario, Long>{
 	Page<Usuario> findAll(Pageable pageable);
 	
+	/*
+	 * Search by user name and state in true
+	 * */
+	
+	public Usuario findByUsuarioAndEstUsuario(String usuario, Boolean estUsuario);
+	
 	@Query (value="SELECT * FROM usuario WHERE usuario=?1 AND contrasenia=?2", nativeQuery = true) 
 	 Usuario login (String usuario, String contrasenia);
+	
+	
 }
