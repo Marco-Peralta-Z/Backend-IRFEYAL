d6417082fe0bd75f6b0666dc3c6c2f32face5fa8
Stevensmb <100179543+Stevensmb@users.noreply.github.com>
2022-06-15 16:28:52 -0500

    update

--- src/main/java/com/irfeyal/modelo/dao/parametrizacionacademica/AsignaturaRepository.java
+++ src/main/java/com/irfeyal/modelo/dao/parametrizacionacademica/AsignaturaRepository.java
@@ -15,15 +15,15 @@ public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
 	/*MODULO ASISTENCIA*/
 	@Query(value="SELECT asig.id_asignatura, asig.descripcion,asig.fecha_creacion  "
 			+ "FROM periodo p join  malla m on p.id_malla = m.id_malla  "
-			+ "	join malla_asignatura ma on ma.id_malla = m.id_malla "
-			+ "	join asignatura asig on asig.id_asignatura = ma.id_asignatura "
-			+ "	join asignatura_empleado asigemple on asigemple.id_asignatura = asig.id_asignatura "
-			+ "	join modalidad mo on m.id_modalidad=mo.id_modalidad "
-			+ "	join malla_curso mc on m.id_malla=mc.id_malla "
-			+ "	join curso c on mc.id_curso=c.id_curso "
-			+ "	join curso_paralelo cp on c.id_curso=cp.id_curso "
-			+ "	join paralelo pa on cp.id_paralelo=pa.id_paralelo "
-			+ "	where asigemple.id_empleado=?1 and p.id_periodo=?2 and mo.id_modalidad=?3 and c.id_curso=?4  and pa.id_paralelo=?5",nativeQuery=true)
+			+ "join malla_asignatura ma on ma.id_malla = m.id_malla "
+			+ "join asignatura asig on asig.id_asignatura = ma.id_asignatura "
+			+ "join asignatura_empleado asigemple on asigemple.id_asignatura = asig.id_asignatura "
+			+ "join modalidad mo on m.id_modalidad=mo.id_modalidad "
+			+ "join malla_curso mc on m.id_malla=mc.id_malla "
+			+ "join curso c on mc.id_curso=c.id_curso "
+			+ "join tutor t on c.id_curso=t.id_curso "
+			+ "join paralelo pa on t.id_paralelo=pa.id_paralelo "
+			+ "where asigemple.id_empleado=?1 and p.id_periodo=?2 and mo.id_modalidad=?3 and c.id_curso=?4 and pa.id_paralelo=?5",nativeQuery=true)
 	List<Asignatura> listarasignaturaasistencia(Long empelado , Long idperiodo,Long idmod, Long idcurso,Long idparalelo);
 	
 	
