0a5d5ac9c4583e91dc1b7a6bc73209547eda833b
abelarordor <77416250+abelarordor@users.noreply.github.com>
2022-07-01 05:10:58 -0500

    update  reportes
    se agrego librerias y funciones para reportes

--- src/main/java/com/irfeyal/servicio/asistencia/AsistenciaServiceImpl.java
+++ src/main/java/com/irfeyal/servicio/asistencia/AsistenciaServiceImpl.java
@@ -1,13 +1,24 @@
 package com.irfeyal.servicio.asistencia;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import com.irfeyal.interfaces.asistencia.IAsistenciaService;
 import com.irfeyal.modelo.asistencia.Asistencia;
+import com.irfeyal.modelo.asistencia.Clase;
 import com.irfeyal.modelo.dao.asistencia.IAsistenciaDao;
 import com.irfeyal.modelo.dao.asistencia.IClaseDao;
 import com.irfeyal.modelo.dao.matricula.IEstudianteDao;
@@ -24,7 +35,19 @@ import com.irfeyal.modelo.parametrizacionacademica.Modalidad;
 import com.irfeyal.modelo.parametrizacionacademica.Paralelo;
 import com.irfeyal.modelo.parametrizacionacademica.Periodo;
 import com.irfeyal.modelo.rolseguridad.Persona;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
+import org.springframework.http.MediaType;
+import java.util.Optional;
+import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
 @Service
 public class AsistenciaServiceImpl implements IAsistenciaService{
 	
@@ -181,7 +204,59 @@ public class AsistenciaServiceImpl implements IAsistenciaService{
 		return asistenciadao.actualizarfiltros(id_mod, id_periodo, id_paralelo, id_asignatura, id_curso, fecha,docente);
 	}
 
-	
+	@NotNull
+	public ResponseEntity<ByteArrayResource> exportInvoice(Long id_estudiante ,Long iddocente){
+		List<Asistencia> asispedf= this.asistenciadao.obtenerIdEstudiante(id_estudiante);
+		
+	    Estudiante estudiantedaoa=this.estudiantedao.findestudianteidpdf(id_estudiante);
+			
+			try {
+			
+				final File file = ResourceUtils.getFile("src/main/resources/PDF/estudiantesreport.jasper");
+				final File imgLogo = ResourceUtils.getFile("src/main/resources/logo.png");
+				final JasperReport report = (JasperReport) JRLoader.loadObject(file);
+
+				final Map<String, Object> parameters = new HashMap<>();
+				parameters.put("id_estudiante", id_estudiante);
+				parameters.put("persoNom", estudiantedaoa.getId_persona().getNombre());
+				parameters.put("persoApe", estudiantedaoa.getId_persona().getApellido());
+				parameters.put("cedula", estudiantedaoa.getId_persona().getCedula());
+		        parameters.put("ds", new JRBeanCollectionDataSource((Collection<?>) this.clasedao.mostrarfechasidpdf(id_estudiante,iddocente)));
+				parameters.put("imgLogo", new FileInputStream(imgLogo));
+				
+				
+				
+				JasperPrint jPrint = JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
+				byte [] reporte = JasperExportManager.exportReportToPdf(jPrint);
+	                        
+	                        String sdf = (new SimpleDateFormat("dd/MM/yyyy")).format(new Date());
+	                        
+				StringBuilder stringBuilder = new StringBuilder().append("InvoicePDF:");
+	                        org.springframework.http.ContentDisposition contentDisposition = org.springframework.http.ContentDisposition.builder("attachment")
+	                                .filename(stringBuilder.append(id_estudiante)
+	                                        .append("generateDate:")
+	                                        .append(sdf)
+	                                        .append(".pdf")
+	                                        .toString())
+	                                        .build();
+				org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+	                        headers.setContentDisposition(contentDisposition);
+	                        return ResponseEntity.ok().contentLength((long) reporte.length)
+	                                .contentType(MediaType.APPLICATION_PDF)
+	                                .headers(headers).body(new ByteArrayResource(reporte));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	        return null;
+		
+	}
+
+
+
+
+
 	
 
 }
