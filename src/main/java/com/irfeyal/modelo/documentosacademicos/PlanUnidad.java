915f4c41487c3633fa492b99556c003866e6d8af
DiegoBerrezueta <diego.berrezueta.est@tecazuay.edu.ec>
2022-07-28 22:00:22 -0500

    Commit
    Documentos Academicos

--- src/main/java/com/irfeyal/modelo/documentosacademicos/PlanUnidad.java
+++ src/main/java/com/irfeyal/modelo/documentosacademicos/PlanUnidad.java
@@ -29,14 +29,27 @@ public class PlanUnidad implements Serializable{
 	private Long id_plan_unidad;
 	
 	private String titulo_unidad;
-	private String objetivos;
-	private String contenidos;
-	private String criterios_evaluacion;
-	private String destrezas;
+	private Date fecha_creacion;
 	private Date fecha_inicio;
 	private Date fecha_fin;
+	private Long num_periodos;
+	private String objetivos;
+	private String criterios_evaluacion;
+	private String destrezas;
+	private String act_experiencia;
+	private String act_reflexion;
+	private String act_conceptualizacion;
+	private String act_aplicacion;
+	private String recursos;
+	private String indicadores;
+	private String tecnicas;
+	private String adaptaciones_curriculares;
+	private String adap_necesidad_educativa;
+	private String especificacion_nesesidad;
 	private String estado;
 	private String observaciones;
+	private String coor_academico;
+	private Date fecha_revision;
 	
 	@ManyToOne
 	@JoinColumn (name = "id_unidad")
@@ -69,22 +82,37 @@ public class PlanUnidad implements Serializable{
 	public PlanUnidad() {
 	}
 
-
-	public PlanUnidad(Long id_plan_unidad, String titulo_unidad, String objetivos, String contenidos,
-			String criterios_evaluacion, String destrezas, Date fecha_inicio, Date fecha_fin, String estado,
-			String observaciones, Unidad unidad, Empleado empleado, Asignatura asignatura, Curso curso,
+	public PlanUnidad(Long id_plan_unidad, String titulo_unidad, Date fecha_creacion, Date fecha_inicio, Date fecha_fin,
+			Long num_periodos, String objetivos, String criterios_evaluacion, String destrezas, String act_experiencia,
+			String act_reflexion, String act_conceptualizacion, String act_aplicacion, String recursos,
+			String indicadores, String tecnicas, String adaptaciones_curriculares, String adap_necesidad_educativa,
+			String especificacion_nesesidad, String estado, String observaciones, String coor_academico,
+			Date fecha_revision, Unidad unidad, Empleado empleado, Asignatura asignatura, Curso curso,
 			Paralelo paralelo, Modalidad modalidad, Periodo periodo) {
 		super();
 		this.id_plan_unidad = id_plan_unidad;
 		this.titulo_unidad = titulo_unidad;
-		this.objetivos = objetivos;
-		this.contenidos = contenidos;
-		this.criterios_evaluacion = criterios_evaluacion;
-		this.destrezas = destrezas;
+		this.fecha_creacion = fecha_creacion;
 		this.fecha_inicio = fecha_inicio;
 		this.fecha_fin = fecha_fin;
+		this.num_periodos = num_periodos;
+		this.objetivos = objetivos;
+		this.criterios_evaluacion = criterios_evaluacion;
+		this.destrezas = destrezas;
+		this.act_experiencia = act_experiencia;
+		this.act_reflexion = act_reflexion;
+		this.act_conceptualizacion = act_conceptualizacion;
+		this.act_aplicacion = act_aplicacion;
+		this.recursos = recursos;
+		this.indicadores = indicadores;
+		this.tecnicas = tecnicas;
+		this.adaptaciones_curriculares = adaptaciones_curriculares;
+		this.adap_necesidad_educativa = adap_necesidad_educativa;
+		this.especificacion_nesesidad = especificacion_nesesidad;
 		this.estado = estado;
 		this.observaciones = observaciones;
+		this.coor_academico = coor_academico;
+		this.fecha_revision = fecha_revision;
 		this.unidad = unidad;
 		this.empleado = empleado;
 		this.asignatura = asignatura;
@@ -93,183 +121,251 @@ public class PlanUnidad implements Serializable{
 		this.modalidad = modalidad;
 		this.periodo = periodo;
 	}
-
 
 	public Long getId_plan_unidad() {
 		return id_plan_unidad;
 	}
 
-
 	public void setId_plan_unidad(Long id_plan_unidad) {
 		this.id_plan_unidad = id_plan_unidad;
 	}
-
 
 	public String getTitulo_unidad() {
 		return titulo_unidad;
 	}
 
-
 	public void setTitulo_unidad(String titulo_unidad) {
 		this.titulo_unidad = titulo_unidad;
 	}
 
-
-	public String getObjetivos() {
-		return objetivos;
+	public Date getFecha_creacion() {
+		return fecha_creacion;
 	}
 
-
-	public void setObjetivos(String objetivos) {
-		this.objetivos = objetivos;
+	public void setFecha_creacion(Date fecha_creacion) {
+		this.fecha_creacion = fecha_creacion;
 	}
-
-
-	public String getContenidos() {
-		return contenidos;
-	}
-
-
-	public void setContenidos(String contenidos) {
-		this.contenidos = contenidos;
-	}
-
-
-	public String getCriterios_evaluacion() {
-		return criterios_evaluacion;
-	}
-
-
-	public void setCriterios_evaluacion(String criterios_evaluacion) {
-		this.criterios_evaluacion = criterios_evaluacion;
-	}
-
-
-	public String getDestrezas() {
-		return destrezas;
-	}
-
-
-	public void setDestrezas(String destrezas) {
-		this.destrezas = destrezas;
-	}
-
 
 	public Date getFecha_inicio() {
 		return fecha_inicio;
 	}
 
-
 	public void setFecha_inicio(Date fecha_inicio) {
 		this.fecha_inicio = fecha_inicio;
 	}
-
 
 	public Date getFecha_fin() {
 		return fecha_fin;
 	}
 
-
 	public void setFecha_fin(Date fecha_fin) {
 		this.fecha_fin = fecha_fin;
 	}
 
+	public Long getNum_periodos() {
+		return num_periodos;
+	}
+
+	public void setNum_periodos(Long num_periodos) {
+		this.num_periodos = num_periodos;
+	}
+
+	public String getObjetivos() {
+		return objetivos;
+	}
+
+	public void setObjetivos(String objetivos) {
+		this.objetivos = objetivos;
+	}
+
+	public String getCriterios_evaluacion() {
+		return criterios_evaluacion;
+	}
+
+	public void setCriterios_evaluacion(String criterios_evaluacion) {
+		this.criterios_evaluacion = criterios_evaluacion;
+	}
+
+	public String getDestrezas() {
+		return destrezas;
+	}
+
+	public void setDestrezas(String destrezas) {
+		this.destrezas = destrezas;
+	}
+
+	public String getAct_experiencia() {
+		return act_experiencia;
+	}
+
+	public void setAct_experiencia(String act_experiencia) {
+		this.act_experiencia = act_experiencia;
+	}
+
+	public String getAct_reflexion() {
+		return act_reflexion;
+	}
+
+	public void setAct_reflexion(String act_reflexion) {
+		this.act_reflexion = act_reflexion;
+	}
+
+	public String getAct_conceptualizacion() {
+		return act_conceptualizacion;
+	}
+
+	public void setAct_conceptualizacion(String act_conceptualizacion) {
+		this.act_conceptualizacion = act_conceptualizacion;
+	}
+
+	public String getAct_aplicacion() {
+		return act_aplicacion;
+	}
+
+	public void setAct_aplicacion(String act_aplicacion) {
+		this.act_aplicacion = act_aplicacion;
+	}
+
+	public String getRecursos() {
+		return recursos;
+	}
+
+	public void setRecursos(String recursos) {
+		this.recursos = recursos;
+	}
+
+	public String getIndicadores() {
+		return indicadores;
+	}
+
+	public void setIndicadores(String indicadores) {
+		this.indicadores = indicadores;
+	}
+
+	public String getTecnicas() {
+		return tecnicas;
+	}
+
+	public void setTecnicas(String tecnicas) {
+		this.tecnicas = tecnicas;
+	}
+
+	public String getAdaptaciones_curriculares() {
+		return adaptaciones_curriculares;
+	}
+
+	public void setAdaptaciones_curriculares(String adaptaciones_curriculares) {
+		this.adaptaciones_curriculares = adaptaciones_curriculares;
+	}
+
+	public String getAdap_necesidad_educativa() {
+		return adap_necesidad_educativa;
+	}
+
+	public void setAdap_necesidad_educativa(String adap_necesidad_educativa) {
+		this.adap_necesidad_educativa = adap_necesidad_educativa;
+	}
+
+	public String getEspecificacion_nesesidad() {
+		return especificacion_nesesidad;
+	}
+
+	public void setEspecificacion_nesesidad(String especificacion_nesesidad) {
+		this.especificacion_nesesidad = especificacion_nesesidad;
+	}
 
 	public String getEstado() {
 		return estado;
 	}
 
-
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-
 
 	public String getObservaciones() {
 		return observaciones;
 	}
 
-
 	public void setObservaciones(String observaciones) {
 		this.observaciones = observaciones;
 	}
 
+	public String getCoor_academico() {
+		return coor_academico;
+	}
+
+	public void setCoor_academico(String coor_academico) {
+		this.coor_academico = coor_academico;
+	}
+
+	public Date getFecha_revision() {
+		return fecha_revision;
+	}
+
+	public void setFecha_revision(Date fecha_revision) {
+		this.fecha_revision = fecha_revision;
+	}
 
 	public Unidad getUnidad() {
 		return unidad;
 	}
 
-
 	public void setUnidad(Unidad unidad) {
 		this.unidad = unidad;
 	}
-
 
 	public Empleado getEmpleado() {
 		return empleado;
 	}
 
-
 	public void setEmpleado(Empleado empleado) {
 		this.empleado = empleado;
 	}
-
 
 	public Asignatura getAsignatura() {
 		return asignatura;
 	}
 
-
 	public void setAsignatura(Asignatura asignatura) {
 		this.asignatura = asignatura;
 	}
-
 
 	public Curso getCurso() {
 		return curso;
 	}
 
-
 	public void setCurso(Curso curso) {
 		this.curso = curso;
 	}
-
 
 	public Paralelo getParalelo() {
 		return paralelo;
 	}
 
-
 	public void setParalelo(Paralelo paralelo) {
 		this.paralelo = paralelo;
 	}
-
 
 	public Modalidad getModalidad() {
 		return modalidad;
 	}
 
-
 	public void setModalidad(Modalidad modalidad) {
 		this.modalidad = modalidad;
 	}
-
 
 	public Periodo getPeriodo() {
 		return periodo;
 	}
 
-
 	public void setPeriodo(Periodo periodo) {
 		this.periodo = periodo;
 	}
-
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
     
-	
 	
 
 }
